4a2c16db9ff51dec17b096fb65ed7f0396c00c29
SGroznica <93283616+SGroznica@users.noreply.github.com>
2022-03-26 20:04:52 +0100

    Kreiran prozor za Red klasu

--- PiramidaZaTenis/src/main/java/piramidazatenis/view/Izbornik.java
+++ PiramidaZaTenis/src/main/java/piramidazatenis/view/Izbornik.java
@@ -94,6 +94,11 @@ public class Izbornik extends javax.swing.JFrame {
         jMenu2.add(jMenuItem2);
 
         jMenuItem3.setText("Red");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
         jMenu2.add(jMenuItem3);
 
         jMenuItem4.setText("Tennis match");
@@ -136,6 +141,10 @@ public class Izbornik extends javax.swing.JFrame {
     private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
        new TennisMatchProzor().setVisible(true);
     }//GEN-LAST:event_jMenuItem4ActionPerformed
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+       new RedProzor().setVisible(true);
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     /**
      * @param args the command line arguments
