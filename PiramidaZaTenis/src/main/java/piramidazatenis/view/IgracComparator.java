29012ec8e17dbdb61d2c906ab4b13f470897e140
SGroznica <93283616+SGroznica@users.noreply.github.com>
2022-04-11 17:51:55 +0200

    Ispravljen igrač komparator

--- PiramidaZaTenis/src/main/java/piramidazatenis/view/IgracComparator.java
+++ PiramidaZaTenis/src/main/java/piramidazatenis/view/IgracComparator.java
@@ -5,6 +5,7 @@
 package piramidazatenis.view;
 
 import java.text.Collator;
+import java.util.Comparator;
 import java.util.Locale;
 import piramidazatenis.model.Igrac;
 
@@ -12,7 +13,7 @@ import piramidazatenis.model.Igrac;
  *
  * @author stjep
  */
-public final class IgracComparator implements Comparable<Igrac>{
+public class IgracComparator implements Comparator<Igrac>{
      private Collator hr;
 
     public IgracComparator() {
@@ -25,8 +26,15 @@ public final class IgracComparator implements Comparable<Igrac>{
         return hr.compare(o1.getPrezime(), o2.getPrezime());
     }
 
+   /*@Override
+    public int compare(Igrac o1, Igrac o2) {
+        Integer prvi = o1.get()== null ? 0 : o1.getPolaznici().size();
+        Integer drugi = o2.getPolaznici() == null ? 0 : o2.getPolaznici().size();
+        return drugi.compareTo(prvi);
+    }*/
+
     @Override
-    public int compareTo(Igrac o) {
+    public int compare(Igrac o1, Igrac o2) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
