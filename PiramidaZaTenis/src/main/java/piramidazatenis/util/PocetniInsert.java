446abdc0acc6e7ee1eb9677c4f85ca9bfb8f980d
SGroznica <93283616+SGroznica@users.noreply.github.com>
2022-04-18 11:23:08 +0200

    Izmjenjen početni insert

--- PiramidaZaTenis/src/main/java/piramidazatenis/util/PocetniInsert.java
+++ PiramidaZaTenis/src/main/java/piramidazatenis/util/PocetniInsert.java
@@ -5,6 +5,8 @@
 package piramidazatenis.util;
 
 import com.github.javafaker.Faker;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
@@ -53,6 +55,19 @@ public class PocetniInsert {
         
     }
     
+     
+      private static Date pocetakMatcha() {
+        Random random = new Random();
+        ZoneId defaultZoneId = ZoneId.systemDefault();
+        int minDay = (int) LocalDate.of(2022, 1, 1).toEpochDay();
+        var maxDay = (int) LocalDate.of(2022, 12, 31).toEpochDay();
+        long randomDay = minDay + random.nextInt(maxDay - minDay);
+
+        LocalDate pMeca = LocalDate.ofEpochDay(randomDay);
+        Date date = Date.from(pMeca.atStartOfDay(defaultZoneId).toInstant());
+        return date;
+    }
+     
      private static void unesiTennisMatch(){
         Session session = HibernateUtil.getSession();
         session.beginTransaction();
@@ -68,7 +83,7 @@ public class PocetniInsert {
                 tm.setIgrac1(i);
                 Collections.shuffle(igraci);
                 tm.setIgrac2(igraci.get((int) Math.random() * (igraci.size() - 26)));
-                tm.setDatumigranja(new Date());
+                tm.setDatumigranja(pocetakMatcha());
                 tm.setTeren(faker.team().name());
                 tm.setRed(i.getRed());
                 session.save(tm);
